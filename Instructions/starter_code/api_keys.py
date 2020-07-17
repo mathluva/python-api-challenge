9f7a94ad9c84cfb5ff99e2324b9f349e0d806a31
mathluva <mathluvacoding@gmail.com>
2020-07-16 21:59:11 -0400

    update heat maps

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "a8fcb29b99f13e282ab80af106906e5f"
+weather_api_key = "ENTER API KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyBeNLpXcceK3k-eJykgJ8lQ6LUqlNRBreQ"
+g_key = "ENTER API KEY HERE"
 
